d386ab6a1357a75af89e86e2b02adaceb0c74854
ggarcesv <garces1989@gmail.com>
2020-01-13 08:54:54 -0300

    Update

--- resources/views/perfil/MisDatosListar.blade.php
+++ resources/views/perfil/MisDatosListar.blade.php
@@ -3,6 +3,16 @@
 
 <title>@yield('title', 'Mis Datos')</title>
 
+@section('header')
+    <h3>Mis Datos</h3>
+@stop
 
-    <p>
-    </p>
+@section('content')
+
+<p> Bienvenido</p>
+
+<p> Tabla con datos del usuario </p>
+
+<p> Tabla con las evaluaciones </p>
+
+@stop
